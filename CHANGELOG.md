92f46456481c187cf0330958c8eac1ba523f5945
Alexandre Bulté <alexandre@bulte.net>
2018-10-02 16:50:01 +0200

    Bump version 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # Changelog
 
-## Current (in progress)
+## 1.0.0 (2018-10-02)
 
 Initial release
--- udata_tabular_preview/__init__.py
+++ udata_tabular_preview/__init__.py
@@ -6,5 +6,5 @@ A plugin to preview tabular files (CSV, Excel, ...)
 '''
 from __future__ import unicode_literals
 
-__version__ = '0.1.0.dev'
+__version__ = '1.0.0'
 __description__ = 'A plugin to preview tabular files (CSV, Excel, ...) using csvapi'
