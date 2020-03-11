74b3f31d516fca01ce2fc6ce42e9434031c7ea14
Alexandre Bulté <alexandre@bulte.net>
2020-03-11 18:16:08 +0100

    Update to version 2.0.1.dev for next development cycle

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## Current (in progress)
+
+- Nothing yet
+
 ## 2.0.0 (2020-03-11)
 
 - Migrate to Python3 [#5](https://github.com/opendatateam/udata-tabular-preview/pull/5)
--- udata_tabular_preview/__init__.py
+++ udata_tabular_preview/__init__.py
@@ -4,5 +4,5 @@ Tabular Preview
 A plugin to preview tabular files (CSV, Excel, ...)
 '''
 
-__version__ = '2.0.0'
+__version__ = '2.0.1.dev'
 __description__ = 'A plugin to preview tabular files (CSV, Excel, ...) using csvapi'
