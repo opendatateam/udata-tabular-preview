45966a9b1bd554a76ae17da407ea6e3be0176962
quaxsze <plum.stanislas@gmail.com>
2022-07-04 17:05:25 +0200

    manifestomy

--- udata_tabular_preview/views.py
+++ udata_tabular_preview/views.py
@@ -11,14 +11,6 @@ blueprint = Blueprint('tabular', __name__, url_prefix='/tabular',
                       static_folder='static')
 
 
-MANIFESTS = {
-    'dataexplorer': os.path.join(blueprint.static_folder,
-                                 'dataexplorer/asset-manifest.json'),
-    'csvapi-front': os.path.join(blueprint.static_folder,
-                                 'csvapi-front/manifest.json'),
-}
-
-
 @blueprint.route('/preview/')
 def preview():
     if current_app.config.get('PREVIEW_MODE') is None:
@@ -30,16 +22,3 @@ def preview():
 def init_preview(state):
     for key, default in DEFAULTS.__dict__.items():
         state.app.config.setdefault(key, default)
-    with state.app.app_context():
-        for key, manifest in MANIFESTS.items():
-            assets.register_manifest(key, filename=manifest)
-
-
-@blueprint.add_app_template_global
-def tabular_manifest(ui, filename):
-    '''
-    Get an UI asset path from its manifest
-    '''
-    static_root = assets.cdn_for('tabular.static', filename=ui, _external=True)
-    asset = assets.from_manifest(ui, filename, raw=True)
-    return '/'.join((static_root, asset))
