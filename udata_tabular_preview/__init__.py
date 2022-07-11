e6a8a55201825982700dd815448ed950b3ef5990
maudetes <maudet.estelle@gmail.com>
2022-07-11 09:34:13 +0200

    Update to version 2.0.4.dev for next development cycle

--- udata_tabular_preview/__init__.py
+++ udata_tabular_preview/__init__.py
@@ -4,5 +4,5 @@ Tabular Preview
 A plugin to preview tabular files (CSV, Excel, ...)
 '''
 
-__version__ = '2.0.3'
+__version__ = '2.0.4.dev'
 __description__ = 'A plugin to preview tabular files (CSV, Excel, ...) using csvapi'
