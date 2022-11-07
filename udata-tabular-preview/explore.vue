f1147f5374254732d977c7cd4e7f0d0daa10daf8
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-11-07 16:56:34 +0100

    feat: :construction: add preview component

---
+++ udata-tabular-preview/explore.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>HELLO</h1>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+
+});
+</script>
---
+++ udata-tabular-preview/index.js
@@ -0,0 +1,4 @@
+import { registerComponent } from "udata-front";
+import Explore from "./explore.vue";
+
+registerComponent("explore", Explore, "udata-tabular-preview", "explore");
