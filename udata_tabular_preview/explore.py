4c55e3785ef1bdd707d1c548efe8b0e245ed22c7
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-11-21 10:20:35 +0100

    feat: :sparkles: use real can_explore

--- udata_tabular_preview/explore.py
+++ udata_tabular_preview/explore.py
@@ -1,26 +1,7 @@
 from flask import current_app
 
+from udata_tabular_preview.preview import can_preview
+
 
 def can_explore(resource):
-    return True
-    has_config = bool(current_app.config.get('TABULAR_CSVAPI_FRONT_URL'))
-
-    supported_mimes = current_app.config.get('TABULAR_SUPPORTED_MIME_TYPES')
-    extras_mime = resource.extras.get('check:headers:content-type')
-    is_supported = (
-        extras_mime in supported_mimes
-        or resource.mime in supported_mimes
-    )
-
-    is_remote = resource.filetype == 'remote'
-    allow_remote = current_app.config.get('TABULAR_ALLOW_REMOTE')
-    is_allowed = allow_remote or not is_remote
-
-    max_size = current_app.config.get('TABULAR_MAX_SIZE')
-    extras_size = resource.extras.get('check:headers:content-length')
-    size_ok = (
-        not max_size
-        or (extras_size or resource.filesize or float("inf")) <= max_size
-    )
-
-    return all((has_config, is_supported, is_allowed, size_ok))
+    return can_preview(resource)
