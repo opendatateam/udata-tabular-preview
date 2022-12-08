57dbbfef5e527798aefe0cedf14784a3038a3f55
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-12-08 16:10:31 +0100

    build: :arrow_up: use @etalab/udata-front-plugins-helper dep

--- udata-tabular-preview/index.js
+++ udata-tabular-preview/index.js
@@ -1,5 +1,5 @@
 import 'vite/modulepreload-polyfill';
-import { registerComponent, registerTranslation } from "udata-front";
+import { registerComponent, registerTranslation } from "@etalab/udata-front-plugins-helper";
 import Explore from "./explore.vue";
 import messages from '@intlify/unplugin-vue-i18n/messages';
 
