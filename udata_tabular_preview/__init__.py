00132fa06069b40578600a6a3ff77f6a6eab7335
quaxsze <plum.stanislas@gmail.com>
2024-01-23 15:58:03 -0300

    Bump version 3.2.0

--- udata_tabular_preview/__init__.py
+++ udata_tabular_preview/__init__.py
@@ -4,5 +4,5 @@ Tabular Preview
 A plugin to preview tabular files (CSV, Excel, ...)
 '''
 
-__version__ = '3.1.0'
+__version__ = '3.2.0'
 __description__ = 'A plugin to preview tabular files (CSV, Excel, ...) using csvapi'
