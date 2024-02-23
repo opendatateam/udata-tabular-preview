b8359a119979bbdf18fae127e2ad71cc212c9420
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2024-02-23 14:55:46 +0100

    fix: parametrize rid

--- udata-tabular-preview/tabularapi.js
+++ udata-tabular-preview/tabularapi.js
@@ -27,8 +27,7 @@ export function fetchData(url) {
  * @returns {object} api result
  */
 export function getData(id, page, sortConfig) {
-  //let url = `${tabular_api_url}/api/resources/${id}/data/?page=${page}&page_size=${tabular_page_size}`; // Construit l'URL avec l'ID
-  let url = `${tabular_api_url}/api/resources/3ab0d13c-1ef0-435d-9af4-519c72f46303/data/?page=${page}&page_size=${tabular_page_size}`; // Construit l'URL avec l'ID
+  let url = `${tabular_api_url}/api/resources/${id}/data/?page=${page}&page_size=${tabular_page_size}`; // Construit l'URL avec l'ID
   
   if (sortConfig) {
     url = url + `&${sortConfig.column}__sort=${sortConfig.type}`
@@ -42,7 +41,6 @@ export function getData(id, page, sortConfig) {
  * @returns {object} api result
  */
 export function getProfile(id) {
-  //let url = `${tabular_api_url}/api/resources/${id}/profile/`; // Construit l'URL avec l'ID
-  let url = `${tabular_api_url}/api/resources/91084b11-1316-422e-bccf-b86234cd3fd9/profile/`; // Construit l'URL avec l'ID
+  let url = `${tabular_api_url}/api/resources/${id}/profile/`; // Construit l'URL avec l'ID
   return fetchData(url)
 }
